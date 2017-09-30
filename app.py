1f401e5bfc5285fe26888a1ba25e2d0d1e8e6f1c
Flávio Silvestre <flaviosilvestre89@gmail.com>
2017-09-30 11:58:41 +0000

    save

--- app.py
+++ app.py
@@ -1,29 +1,50 @@
-from flask import Flask, request
+from flask import Flask
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
+from sqlalchemy import Table, ForeignKey, MetaData, Column, String, Integer, create_engine
 from json import dumps
-
-db = create_engine('sqlite:///salaries.db')
+import os
 
 app = Flask(__name__)
 api = Api(app)
 
-class Departments_Meta(Resource):
+current_dir = os.path.dirname(os.path.abspath(__file__))
+db = create_engine('sqlite:///' + current_dir + '/main.db')
+
+metadata = MetaData()
+
+users = Table('users', metadata,
+    Column('user_id', Integer, primary_key=True,  autoincrement=True),
+    Column('user_name', String(16), nullable=False),
+    Column('email_address', String(60), key='email'),
+)
+
+meetings = Table('meetings', metadata,
+    Column('meeting_id', Integer, primary_key=True, autoincrement=True),
+    Column('owner', Integer, ForeignKey("users.user_id"), nullable=False),
+    Column('url', String(40), nullable=False),
+    Column('privacy', String(100)) ## TODO
+)
+
+metadata.create_all(db)
+
+
+class Users(Resource):
     def get(self):
         conn = db.connect()
-        query = conn.execute("select distinct DEPARTMENT from salaries")
-        return {'departments': [i[0] for i in query.cursor.fetchall()]}
+        query = conn.execute("select distinct user_name from users")
+        return {'users': [i[0] for i in query.cursor.fetchall()]}
 
-class Departmental_Salary(Resource):
-    def get(self, department_name):
+class User(Resource):
+    def get(self, user_id):
         conn = db.connect()
-        query = conn.execute("select * from salaries where Department='%s'"%department_name.upper())
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        query = conn.execute("select * from users where user_id='%d'"%user_id)
+        result = {'user': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return result
 
-       
-api.add_resource(Departmental_Salary, '/dept/<string:department_name>')
-api.add_resource(Departments_Meta, '/departments')
+
+
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(Users, '/users')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ client.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+jsonheader = {'Content-type': 'application/json'}
+
+def get_tasks():
+    r = requests.get("http://127.0.0.1:5000/users")
+    dumpRequest(r)
+
+
+def post_task():
+    url = "http://127.0.0.1:5000/users"
+    data = {"user_id": "1"}
+    data_json = json.dumps(data)
+    response = requests.post(url, data=data_json, headers=jsonheader)
+
+def dumpRequest(request):
+    print(request.headers)
+    print(request.json())
+
+
+
+if __name__ == '__main__':
+    post_task()
+
+
