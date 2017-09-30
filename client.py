77a6a94ceadc2d58bf0fd4da9c27371b0b37e3f6
Flávio Silvestre <flaviosilvestre89@gmail.com>
2017-09-30 13:04:29 +0000

    ++

--- client.py
+++ client.py
@@ -3,10 +3,25 @@ import json
 
 jsonheader = {'Content-type': 'application/json'}
 
-def get_all_users():
-    r = requests.get("http://127.0.0.1:5000/users")
-    dumpRequest(r)
+def create_user(user_name, user_email):
+    url = "http://127.0.0.1:5000/users"
+    data = {"email": user_email, "name": user_name}
+    data_json = json.dumps(data)
+    response = requests.post(url, data=data_json, headers=jsonheader)
 
+def create_conference(owner, recording_url, privacy):
+    return 0
+
+def delete_conference(meeting_id):
+    return 0
+
+def share_meeting(meeting_id, user_email):
+    return 0
+
+
+def get_all(resource):
+    r = requests.get("http://127.0.0.1:5000/" + resource)
+    dumpRequest(r)
 
 def create_user(user_name, user_email):
     url = "http://127.0.0.1:5000/users"
@@ -20,7 +35,7 @@ def dumpRequest(request):
     print(request.json())
 
 if __name__ == '__main__':
-    get_all_users()
+    get_all("users")
     create_user("Flavio", "flaviosilvestre89@gmail.com")
 
 
