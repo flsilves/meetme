e91d97f3adc3a380cec681168c7c104ab1fa2442
flsilves <flavio.silvestre@nokia.com>
2017-10-02 18:35:02 +0100

    SAVEPOINT

--- tests.py
+++ tests.py
@@ -1,21 +1,24 @@
 import app
 from models import *
-import unittest, requests
+import unittest
 
 class BasicTestCase(unittest.TestCase):
         def setUp(self):
             self.app = app.create_app()
             self.client = self.app.test_client()
             self.db = create_engine(DB_URI)
+            Base.metadata.drop_all(self.db)
+            Base.metadata.create_all(self.db)
 
         def tearDown(self):
             pass
 
         def test_create_user(self):
             response = self.client.get('http://localhost:5000/users')
-            print((response.data).decode('utf-8'))
+            payload = (response.data).decode('utf-8')
             print("Status Code = {}".format(response.status_code))
-            #self.assertEqual(response.json(), { '': ''})
+            self.assertEqual(payload, "[]\n")
+            self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
